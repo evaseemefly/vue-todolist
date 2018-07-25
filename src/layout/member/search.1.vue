e38f9e8f4c0d4dfefe32a47c816efdc7b59cc2ac
sihan <evaseemefly@126.com>
2018-07-25 10:49:10 +0800

    加入搜索条

--- src/layout/member/search.1.vue
+++ src/layout/member/search.1.vue
@@ -3,36 +3,52 @@
 
 <template>
 
-  <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                    aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">查询条件</a>
+  <div class="panel-body" style="padding-bottom:0px;">
+    <div id="panel_search" class="panel panel-primary">
+      <div class="panel-heading">查询条件</div>
+      <div class="panel-body table-parent-panel">
+        <form id="formSearch" class="form-horizontal">
+          <div class="form-group" style="margin-top:15px">
+            <label class="control-label col-sm-1" for="txt_search_departmentname">姓名</label>
+            <div class="col-md-2">
+              <select class="form-control" v-model="selected_user">
+                <option v-for='option in options_user' v-bind:value="option.value">
+                  {{option.text}}
+                </option>
+              </select>
             </div>
-            <form class="navbar-form navbar-left">
-                <div class="row">
-                    <div class="form-group col-md-4">
-                        姓名
-                        <input type="text"  name="" id="" class="form-control">
-                    </div>
-                    <div class="form-group col-md-4">
-                        日期
-                        <input type="text" class="form-control picker" readonly>
-                    </div>
-                    <button type="submit" class="btn btn-default mt-5">查询</button>
+            <label class="control-label col-sm-1" for="txt_search_departmentname">组名</label>
+            <div class="col-md-2">
+              <select class="form-control" v-model="selected_group">
+                <option v-for='option in options_group' v-bind:value="option.value">
+                  {{option.text}}
+                </option>
+              </select>
+            </div>
+            <label class="control-label col-sm-1" for="txt_search_statu">日期</label>
+            <div class="col-md-2">
+              <div class="form-group">
+                <div class="input-group date form_date col-md-12" data-date="" data-date-format="yyyy MM dd  " data-link-field="dtp_input2"
+                  data-link-format="yyyy-mm-dd">
+                  <input id="datetimepicker" class="form-control" size="16" type="text" value="">
+                  <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"></span>
+                  </span>
                 </div>
-            </form>
+                <input type="hidden" id="dtp_input2" value="" />
+                <br/>
+              </div>
+            </div>
+            <div class="col-md-2" style="text-align:left;">
+              <!-- <button type="submit" id="btn_search" type="button" style="margin-left:50px" id="btn_query" class="btn btn-primary">查询</button> -->
+              <button type="button" id="btn_search" style="margin-left:50px" v-on:click="summit()" class="btn btn-primary">查询</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
 
-        </div>
-
-    </nav>
+  </div>
 
 </template>
 
